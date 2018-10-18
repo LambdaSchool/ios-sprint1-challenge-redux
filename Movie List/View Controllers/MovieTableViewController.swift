0f43b9cdf032b9f16c66cc61ec97429b9cd10aa6
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-18 12:55:26 -0700

    created TableViewCell

--- Movie List/View Controllers/MovieTableViewController.swift
+++ Movie List/View Controllers/MovieTableViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class MovieTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // connect the view controller
+    // connect conform to delegate
+    // connect to datasource
+    
+    let reusueIdentifier = "cell"
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return MovieController.shared.movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = x
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusueIdentifier) as UITableViewCell? 
         
         return cell
     }
