e896ef63b8b9aec4f7256002a4205ee3d448e879
Lotanna <sugabelly@gmail.com>
2018-10-18 18:02:31 -0400

    code hooked up

--- Movie List/Movie List/MovieListViewController.swift
+++ Movie List/Movie List/MovieListViewController.swift
@@ -8,8 +8,69 @@
 
 import UIKit
 
-class MovieListViewController: UIViewController {
+class MovieListViewController: UITableViewController {
 
+    @IBOutlet weak var movieTable: UITableView!
+    @IBOutlet weak var movieTitle: UILabel!
+    
+    
+   //Create a row
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+        return MovieCollection.count
+        
+    }
+    
+   //Set the contents of the prototype cell to be the Movie whose index matches the current row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let movieCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "singleMovie")
+        
+        movieTitle.text = MovieCollection[IndexPath.row].title
+    
+    }
+    
+    
+    //Change the Not Seen button to Seen when selected and vice versa
+    @IBAction func movieStatus(_ sender: UIButton) {
+        
+        if MovieCollection[indexPath.row].seen == true {
+            
+            MovieCollection[indexPath.row].seen = false
+            
+        }
+        
+        if MovieCollection[indexPath.row].seen == false {
+            
+            MovieCollection[indexPath.row].seen = true
+            
+        }
+    }
+    
+    
+    //Allow the row to be deleted by swiping
+    
+    
+    
+    //Reload the view to show the updated information
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Default Code, leave alone
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- Movie List/Movie List/NewMovieViewController.swift
+++ Movie List/Movie List/NewMovieViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class NewMovieViewController: UIViewController {
 
+    @IBOutlet weak var enteredMovie: UITextField!
+    
+    @IBAction func newMovieButton(_ sender: UIButton) {
+    
+    //Use the content of newMovieButton to create an instance of Movie
+        func createMovie(moviename: String) -> Movie {
+        moviename = enteredMovie.text
+        
+        let newMovie = Movie(title: moviename)
+        
+        MovieCollection.append(newMovie)  //append it to Movie Collection
+        enteredMovie.text = ""    //Reset the input so you can add another movie
+
+        
+        return newMovie
+        }
+    }
+    
+    //Default Code, leave alone
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
